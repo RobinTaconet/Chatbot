88784624c1aedf61dfc38d7395868ee84f809f7c
Robin Taconet <rtaconet@rtaconet-ltm.internal.salesforce.com>
2017-10-26 21:07:07 +0200

    webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var chatService = require('server/chatService')
+var chatService = require('../server/chatService')
 
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
